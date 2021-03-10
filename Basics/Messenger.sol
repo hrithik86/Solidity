8a04dea3cbbb09ee86604062831bee22aa167026
hrithik86 <hrithikmalik86@gmail.com>
2021-03-10 21:16:54 +0530

    First smart contract added

---
+++ Basics/Messenger.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.4.24;
+
+contract Messenger{
+    address owner;
+    string[] messages;
+    
+    constructor() public {
+        owner = msg.sender;
+    }
+    
+    function add(string newMessage) public {
+        require(msg.sender == owner);
+        messages.push(newMessage);
+    }
+    
+    function count() view public returns(uint){
+        return messages.length;
+    }
+    
+    function getMessages(uint index) view public returns(string){
+        return messages[index];
+    }
+}
