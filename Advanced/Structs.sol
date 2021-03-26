74afcde4d7edb3eaf9045857cbb393dce3db1fc0
hrithik86 <hrithikmalik86@gmail.com>
2021-03-26 22:00:58 +0530

    Structs

---
+++ Advanced/Structs.sol
@@ -0,0 +1,20 @@
+pragma solidity^0.4.24;
+
+contract Bank {
+ struct Account {
+ address addr;
+ uint amount;
+ }
+
+ Account public acc = Account({
+     addr: 0x66ec542D55a86F2Fd0B0d9cB9f31bc20aC02477a,
+     amount: 50
+ });
+
+ function addAmount(uint _addMoney) public {
+    acc.amount += _addMoney;
+ }
+ function getAmount() view public returns(uint){
+     return acc.amount;
+ }
+}
