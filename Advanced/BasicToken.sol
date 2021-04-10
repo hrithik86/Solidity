cb3f9dcae0c4a41fcbc1f4b94e14d144c0018b26
hrithik86 <hrithikmalik86@gmail.com>
2021-04-10 13:38:21 +0530

    Basic Token in Solidity

---
+++ Advanced/BasicToken.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.24;
+
+contract BasicToken{
+    mapping(address=>uint256) public balanceOf;
+    constructor(uint initialSupply) public{
+        balanceOf[msg.sender]=initialSupply;
+    }
+    function transfer(address _to,uint256 _value) public returns(bool success){
+        require(balanceOf[msg.sender]>=_value);
+        require(balanceOf[_to]+_value>=balanceOf[_to]);
+        balanceOf[msg.sender]-=_value;
+        balanceOf[_to]+=_value;
+        return true;
+    }
+}
